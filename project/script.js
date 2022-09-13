d417a7029bd45c468a9010d8c3d0f7fe69692204
Kostya <example@gmail.com>
2022-09-13 23:00:48 +0300

    for and if practice

--- project/script.js
+++ project/script.js
@@ -1,15 +1,24 @@
-const numberOfFilms = prompt("How many films did you watch?", "5"),
-      nameOfTheMovie = prompt("Name of the last movie you watched?", "Titanic"),
-      userRate = prompt("Your rate? (0 - 10)", "10");
+// let movieName = prompt("Name of the last movie you watched?", "Titanic"),
+//     userRate = prompt("Your rate? (0 - 10)", "10");
 
 const personaMovieDB = {
-    count: +numberOfFilms,
-    movies: {
-        [nameOfTheMovie]: +userRate
-    },
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-console.log(personaMovieDB.movies);
+let counter = 0;
+
+for (let i = 0; i <= counter; i++) {
+   const movieName = prompt("Name of the last movie you watched?", "Titanic"),
+   userRate = prompt("Your rate? (0 - 10)", "10");
+    if (movieName != '' && userRate != '' && movieName.length < 50) {
+        personaMovieDB.movies[movieName] = +userRate;
+        console.log(personaMovieDB);
+        counter--;
+    } else {
+        console.log("Error, try it again!");
+        counter++;
+    }
+}
