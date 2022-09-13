6a9012308aa33656b25107b9ade4376b552a6988
Kostya <example@gmail.com>
2022-09-13 23:56:33 +0300

    correct

--- project/script.js
+++ project/script.js
@@ -13,8 +13,9 @@ let counter = 0;
 for (let i = 0; i <= counter; i++) {
    const movieName = prompt("Name of the last movie you watched?", "Titanic"),
    userRate = prompt("Your rate? (0 - 10)", "10");
-    if (movieName != '' && userRate != '' && movieName.length < 50) {
-        personaMovieDB.movies[movieName] = +userRate;
+    if (movieName != '' && movieName != null && userRate != '' && 
+       userRate != null && movieName.length < 50) {
+        personaMovieDB.movies[movieName] = userRate;
         console.log(personaMovieDB);
         counter--;
     } else {
