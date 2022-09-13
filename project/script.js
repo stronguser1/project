a41a2ed06102a3170f3177287134613a4631d146
Kostya <example@gmail.com>
2022-09-13 19:09:07 +0300

    end of end

--- project/script.js
+++ project/script.js
@@ -1,8 +1,6 @@
-const numberOfFilms = prompt("How many films did you watch?", "5");
-
-const nameOfTheMovie = prompt("Name of the last movie you watched?", "Titanic");
-
-const userRate = prompt("Your rate? (0 - 10)", "10");
+const numberOfFilms = prompt("How many films did you watch?", "5"),
+      nameOfTheMovie = prompt("Name of the last movie you watched?", "Titanic"),
+      userRate = prompt("Your rate? (0 - 10)", "10");
 
 const personaMovieDB = {
     count: +numberOfFilms,
