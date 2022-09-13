fa91ae5e557a513a49d1e5045615d780e9a0bc5e
Kostya <example@gmail.com>
2022-09-13 19:06:03 +0300

    work end

--- project/script.js
+++ project/script.js
@@ -1,9 +1,17 @@
-let numberOfFilms = prompt("How many films did you watch?", "5");
+const numberOfFilms = prompt("How many films did you watch?", "5");
 
-let personaMovieDB = {
-    count: numberOfFilms,
-    movies: {},
+const nameOfTheMovie = prompt("Name of the last movie you watched?", "Titanic");
+
+const userRate = prompt("Your rate? (0 - 10)", "10");
+
+const personaMovieDB = {
+    count: +numberOfFilms,
+    movies: {
+        [nameOfTheMovie]: +userRate
+    },
     actors: {},
     genres: [],
     privat: false
 };
+
+console.log(personaMovieDB.movies);
