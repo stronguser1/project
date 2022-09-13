03cf2f3df981ae31e64caa34abd1a0757c207d7c
Kostya <example@gmail.com>
2022-09-13 18:49:46 +0300

    personalMovieDB object

--- project/script.js
+++ project/script.js
@@ -1,1 +1,9 @@
 let numberOfFilms = prompt("How many films did you watch?", "5");
+
+let personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
